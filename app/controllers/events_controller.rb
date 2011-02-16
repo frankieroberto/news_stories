284cf46eaa006c7e56c47559209a7c76595ca59b
Frankie Roberto <frankie@frankieroberto.com>
2011-02-16 22:47:01 +0000

    Story view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,31 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+  
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def new
+    @event = Event.new
+    @story = Story.find(params[:story_id]) if params[:story_id]
+  end
+  
+  def create
+    @event = Event.new(params[:event])
+    
+    if params[:story_id]
+      @story = Story.find(params[:story_id])
+      @event.stories << @story
+    end
+    
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
 end
