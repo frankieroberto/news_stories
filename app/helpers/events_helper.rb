79dd1761b4ad7e90bfa2fc0e2dc677a6f21bdf36
Frankie Roberto <frankie@frankieroberto.com>
2011-02-20 11:50:47 +0000

    Basic styling

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,5 +6,15 @@ module EventsHelper
     dates += " – " + event.end.to_s(:short) unless event.start == event.end
     return dates
   end
+  
+  def stories(event)
+    stories = []
+
+    event.stories.each do |story|
+      stories << link_to(story.name, story)
+    end
+    
+    content_tag("h1", stories.join(", ").html_safe, :class => :stories)
+  end
 
 end
