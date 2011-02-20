96d69eea1a4225816bd2650df022a2c8ae0bd5ac
Frankie Roberto <frankie@frankieroberto.com>
2011-02-20 15:58:04 +0000

    Optimisations

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,6 @@
 class HomepageController < ApplicationController
 
   def show
-    @stories = Story.all
     
     if current_user
       @stories_following = current_user.followed_stories
@@ -9,7 +8,7 @@ class HomepageController < ApplicationController
       @stories_following.each do |s|
         stories_following_ids << s.id
       end
-      @events = Event.all(:joins => :story_connections, :conditions => {:story_connections => {:story_id => stories_following_ids}}, :order => "updated_at DESC")
+      @events = Event.all(:joins => :story_connections, :conditions => {:story_connections => {:story_id => stories_following_ids}}, :order => "updated_at DESC", :include => :stories)
     end
   end
 
