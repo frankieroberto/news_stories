3715260457ee565a81f66311c2e8425bdaa9a16c
Frankie Roberto <frankie@frankieroberto.com>
2011-02-24 20:06:42 +0000

    Unit test

--- test/unit/previous_story_name_test.rb
+++ test/unit/previous_story_name_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PreviousStoryNameTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
+  should belong_to :story
+  should validate_presence_of :name
+
 end
