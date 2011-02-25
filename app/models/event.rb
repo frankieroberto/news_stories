2b1fff2e277f07d268576b095e23952d39d1252b
Frankie Roberto <frankie@frankieroberto.com>
2011-02-25 13:31:00 +0000

    Show stories in reverse date order but don't filter

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ActiveRecord::Base
   has_many :story_connections
   has_many :stories, :through => :story_connections
   
+  scope :in_reverse_date_order, :order => "start DESC"
   scope :recent, :conditions => {:updated_at => (Time.now - 1.day)...Time.now}, :order => "updated_at DESC"
   
 
