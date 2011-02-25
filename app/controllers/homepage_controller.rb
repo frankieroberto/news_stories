ddd1e364476c797a98eff0c6b0d0c19f800f0739
Frankie Roberto <frankie@frankieroberto.com>
2011-02-25 11:36:13 +0000

    Grouping and re-ordering homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,12 +3,8 @@ class HomepageController < ApplicationController
   def show
     
     if current_user
-      @stories_following = current_user.followed_stories
-      stories_following_ids = []
-      @stories_following.each do |s|
-        stories_following_ids << s.id
-      end
-      @events = Event.all(:joins => :story_connections, :conditions => {:story_connections => {:story_id => stories_following_ids}}, :order => "events.end DESC, events.updated_at DESC", :include => :stories)
+      
+      @updated_stories = current_user.followed_stories.recently_updated
     end
   end
 
